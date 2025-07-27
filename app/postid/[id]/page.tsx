10f3b28e971575a69c42664d484fd22ca821abfe
nios-x <devnios7@gmail.com>
2025-07-28 00:54:37 +0530

    Added Profile

--- app/postid/[id]/page.tsx
+++ app/postid/[id]/page.tsx
@@ -3,75 +3,64 @@ import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import prisma from "@/lib/prisma";
 import ShowPost from "@/components/Posts/ShowPost";
 
-export default async function Page(props: { params: { id: string } }) {
-  const { id } = await props.params;
+// ✅ Correct typing for route params
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: PageProps) {
+  const { id } = params; // ✅ DO NOT use `await` here
+
   const session = await getServerSession(authOptions);
-  if (!session?.user?.email) {
-    return <div>Unauthorized</div>;
-  }
-  //@ts-ignore
+  if (!session?.user?.email) return <div>Unauthorized</div>;
+
   const postId = parseInt(id);
   if (isNaN(postId)) return <div>Invalid Post ID</div>;
+
   const currentUser = await prisma.user.findUnique({
     where: { email: session.user.email },
     select: { id: true },
   });
-  if (!currentUser) {
-    return <div>User not found</div>;
-  }
+
+  if (!currentUser) return <div>User not found</div>;
 
   const post:any = await prisma.post.findUnique({
     where: { id: postId },
     include: {
-      author: {
-        select: {
-          name: true,
-          image: true,
-        },
-      },
+      author: { select: { name: true, image: true } },
       likes: {
-        where: {
-          id: currentUser.id,
-        },
-        select: {
-          id: true,
-        },
+        where: { id: currentUser.id },
+        select: { id: true },
       },
-        comments: {
-      include: {
-        user: {
-          select: {
-            id: true,
-            name: true,
-            image: true,
+      comments: {
+        include: {
+          user: {
+            select: { id: true, name: true, image: true },
           },
         },
       },
-    },
       _count: {
-        select: {
-          likes: true,
-        },
+        select: { likes: true },
       },
     },
   });
 
-  if (!post) {
-    return <div>Post not found</div>;
-  }
+  if (!post) return <div>Post not found</div>;
 
-  const normalizedPost = {
-    id: post.id,
-    title: post.title,
-    createdAt: post.createdAt,
-    isMedia: post.isMedia ?? false,
-    mediaUrl: post.mediaurl ?? null,
-    visibility: post.visibility ?? "Public",
-    author: post.author,
-    isLiked: post.likes.length > 0,
-    likes: post._count.likes,
-    comments: post.comments
-  };
-
-  return <ShowPost {...normalizedPost} />;
+  return (
+    <ShowPost
+      id={post.id}
+      title={post.title}
+      createdAt={post.createdAt}
+      isMedia={post.isMedia ?? false}
+      mediaUrl={post.mediaurl ?? null}
+      visibility={post.visibility ?? "Public"}
+      author={post.author}
+      isLiked={post.likes.length > 0}
+      likes={post._count.likes}
+      comments={post.comments}
+    />
+  );
 }
--- components/Posts/Post.tsx
+++ components/Posts/Post.tsx
@@ -1,130 +1,155 @@
 "use client";
+
 import { DialogDemo } from "./Options";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-    Heart,
-    MessageCircle,
-    Share2,
-    Globe,
-    Users,
+  Heart,
+  MessageCircle,
+  Share2,
+  Globe,
+  Users,
 } from "lucide-react";
-import share from "@/components/Posts/sharecall"
+import share from "@/components/Posts/sharecall";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { formatDistanceToNow } from "date-fns";
 import { Toaster } from "../ui/sonner";
 import { toast } from "sonner";
+
 type PostProps = {
-    id: number;
-    likes: number; 
-    title: string;
-    isMedia?: boolean;
-    mediaUrl?: string|null;
-    visibility: "Public" | "Followers";
-    isLiked:Boolean
-    author: {
-        name: string;
-        image?: string;
-    };
-    createdAt: string;
-    falserounded?:boolean;
+  id: number;
+  likes: number;
+  title: string;
+  isMedia?: boolean;
+  mediaUrl?: string | null;
+  visibility: "Public" | "Followers";
+  isLiked: boolean;
+  author: {
+    name: string;
+    image?: string;
+  };
+  createdAt: string;
+  falserounded?: boolean;
 };
 
 export default function Post({
-    id,
-    title,
-    isMedia,
-    likes,
-    mediaUrl,
-    visibility = "Public",
-    author,
-    createdAt,
-    handleLike,
-    isLiked,
-    redir,
-    falserounded
-}: PostProps&{handleLike:any, redir?:boolean}) {
-    const visibilityIcon = {
-        Public: <Globe className="w-4 h-4 text-blue-500" />,
-        Followers: <Users className="w-4 h-4 text-green-500" />,
-    }[visibility];
+  id,
+  title,
+  isMedia,
+  likes,
+  mediaUrl,
+  visibility = "Public",
+  author,
+  createdAt,
+  handleLike,
+  isLiked,
+  redir,
+  falserounded,
+}: PostProps & { handleLike: any; redir?: boolean }) {
+  const visibilityIcon = {
+    Public: <Globe className="w-4 h-4 text-blue-500" />,
+    Followers: <Users className="w-4 h-4 text-green-500" />,
+  }[visibility];
 
-    return (
-        <div className="rounded-3xl mb-5 border-zinc-200  border p-4 w-full max-w-xl mx-auto space-y-2 transition-all duration-300   bg-background shadow-xs  dark:bg-input/30 dark:border-input ">
-            <div className="flex items-start justify-between">
-                <div className="w-max flex items-center">
-                    <div className="relative w-14 h-14 rounded-full overflow-hidden shrink-0">
-                        <Toaster/>
-                        <Image
-                            alt="author"
-                                          src={
+  return (
+    <div className="rounded-3xl mb-5 border-zinc-200 border p-4 w-full max-w-xl mx-auto space-y-2 transition-all duration-300 bg-background shadow-xs dark:bg-input/30 dark:border-input">
+      <div className="flex items-start justify-between">
+        <div className="w-max flex items-center">
+          <div className="relative w-14 h-14 rounded-full overflow-hidden shrink-0">
+            <Toaster />
+            <Image
+              alt="author"
+              src={
                 author.image ||
-                "https://ui-avatars.com/api/?name=" + encodeURIComponent(author.name)
+                "https://ui-avatars.com/api/?name=" +
+                  encodeURIComponent(author.name)
               }
-                            fill
-                            
-                            style={{ objectFit: "cover" }}
-                        />
-                    </div>
-                    <div className="flex-1 flex flex-col px-2 pb-2">
-                        <div className="font-semibold text-lg text-zinc-700">{author.name}</div>
-                        <div className="text-xs text-zinc-500">
-                            {formatDistanceToNow(new Date(createdAt), { addSuffix: true })}
-                        </div>
-                    </div>
-                </div>
-
-                {/* Visibility */}
-                <div className="flex items-center space-x-1 text-xs text-zinc-500 px-5 py-3">
-                    {visibilityIcon}
-                    <span>{visibility}</span>
-                    <DialogDemo/>
-                </div>
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className="flex-1 flex flex-col px-2 pb-2">
+            <div className="font-semibold text-lg text-zinc-700">
+              {author.name}
             </div>
-            {redir?<Link href={`${window.location.origin}/postid/${id}`}>
-            {/* Post Content */}
-            <div className="text-[14.6px] px-2 text-zinc-800 whitespace-pre-wrap">{title}</div>
-            </Link>:<div className="text-[14.6px] px-2 text-zinc-800 whitespace-pre-wrap">{title}</div>}
-                                        
-         {isMedia && mediaUrl && (
-                <Link href={mediaUrl}>
-                <div className="w-full h-[400px] relative  mt-5 overflow-hidden">
-                    <Image
-                    src={mediaUrl}
-                    alt="post media"
-                    fill
-                    quality={30}
-                    priority={false}
-                    className="object-contain w-full h-full rounded-md overflow-hidden "
-                    />
-                </div>
-                </Link>
-                )}
-            <div className="flex items-center justify-evenly  w-full gap-2 pt-3  px-1.5 text-zinc-600">
-                <button
-                    onClick={()=>handleLike(id, !isLiked)}
-                    type="button"
-                    aria-label="Love"
-                    className={`${isLiked?"text-white bg-rose-500":"text-rose-400 bg-zinc-100"} transition gap-1  py-2 px-6 rounded-full w-1/3 flex justify-center items-center`}
-                >
-                    <Heart className="w-5 h-5 " strokeWidth={2.4}  /> <span className="text-sm">{likes?likes:""}</span>
-                </button>
-                <Link href={`${window.location.origin}/postid/${id}`} className="text-blue-400 transition bg-zinc-100  py-2 px-6 rounded-full w-1/3 flex justify-center">
-                
-                    <MessageCircle className="w-5 h-5 " strokeWidth={2.4}  />
-                
-                </Link>
-
-                <button onClick={()=> share(id)}
-                    type="button"
-                    aria-label="Share"
-                    className="text-purple-400 transition bg-zinc-100  py-2 px-6 rounded-full w-1/3 flex justify-center"
-                >
-                    <Share2 className="w-5 h-5 " strokeWidth={2.4}  />
-                </button>
+            <div className="text-xs text-zinc-500">
+              {formatDistanceToNow(new Date(createdAt), {
+                addSuffix: true,
+              })}
             </div>
-
+          </div>
         </div>
-    );
+
+        {/* Visibility */}
+        <div className="flex items-center space-x-1 text-xs text-zinc-500 px-5 py-3">
+          {visibilityIcon}
+          <span>{visibility}</span>
+          <DialogDemo />
+        </div>
+      </div>
+
+      {/* Post Content */}
+      {redir ? (
+        <Link href={`/postid/${id}`}>
+          <div className="text-[14.6px] px-2 text-zinc-800 whitespace-pre-wrap">
+            {title}
+          </div>
+        </Link>
+      ) : (
+        <div className="text-[14.6px] px-2 text-zinc-800 whitespace-pre-wrap">
+          {title}
+        </div>
+      )}
+
+      {/* Media */}
+      {isMedia && mediaUrl && (
+        <Link href={mediaUrl}>
+          <div className="w-full h-[400px] relative mt-5 overflow-hidden">
+            <Image
+              src={mediaUrl}
+              alt="post media"
+              fill
+              quality={30}
+              priority={false}
+              className="object-contain w-full h-full rounded-md overflow-hidden"
+            />
+          </div>
+        </Link>
+      )}
+
+      {/* Buttons */}
+      <div className="flex items-center justify-evenly w-full gap-2 pt-3 px-1.5 text-zinc-600">
+        <button
+          onClick={() => handleLike(id, !isLiked)}
+          type="button"
+          aria-label="Love"
+          className={`${
+            isLiked
+              ? "text-white bg-rose-500"
+              : "text-rose-400 bg-zinc-100"
+          } transition gap-1 py-2 px-6 rounded-full w-1/3 flex justify-center items-center`}
+        >
+          <Heart className="w-5 h-5" strokeWidth={2.4} />
+          <span className="text-sm">{likes ?? ""}</span>
+        </button>
+
+        <Link
+          href={`/postid/${id}`}
+          className="text-blue-400 transition bg-zinc-100 py-2 px-6 rounded-full w-1/3 flex justify-center"
+        >
+          <MessageCircle className="w-5 h-5" strokeWidth={2.4} />
+        </Link>
+
+        <button
+          onClick={() => share(id)}
+          type="button"
+          aria-label="Share"
+          className="text-purple-400 transition bg-zinc-100 py-2 px-6 rounded-full w-1/3 flex justify-center"
+        >
+          <Share2 className="w-5 h-5" strokeWidth={2.4} />
+        </button>
+      </div>
+    </div>
+  );
 }
